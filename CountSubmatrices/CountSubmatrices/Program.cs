56de208cff539904772ffe7d0b90e473cf34aeb6
IPPILI PRANAY KUMAR <pranayippili@gmail.com>
2025-08-23 23:05:30 +0530

    sub matrix sum

---
+++ CountSubmatrices/CountSubmatrices/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace CountSubmatrices
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Example input
+			int[,] grid = {
+			{ 7, 6, 3 },
+			{ 6, 6, 1 }
+		};
+			int k = 18;
+
+			int result = CountSubmatrices(grid, k);
+			Console.WriteLine("Output: " + result); // Expected: 4
+		}
+
+		public static int CountSubmatrices(int[,] grid, int k)
+		{
+			int m = grid.GetLength(0);
+			int n = grid.GetLength(1);
+
+			long[,] prefix = new long[m, n];
+			int count = 0;
+
+			for (int i = 0; i < m; i++)
+			{
+				for (int j = 0; j < n; j++)
+				{
+					long sum = grid[i, j];
+
+					if (i > 0) sum += prefix[i - 1, j];
+					if (j > 0) sum += prefix[i, j - 1];
+					if (i > 0 && j > 0) sum -= prefix[i - 1, j - 1];
+
+					prefix[i, j] = sum;
+
+					if (sum <= k)
+						count++;
+				}
+			}
+
+			return count;
+		}
+	}
+}
